37d75e787408f19e1ce517686cc7169c8661d838
Macário Soares <macario.junior@gmail.com>
2016-06-08 11:56:22 -0300

    Ajustando css da pagina de cadastro de usuario e as rotas

--- application/views/cliente/cadastrar.php
+++ application/views/cliente/cadastrar.php
@@ -47,61 +47,60 @@
     );
 ?>
 
-<div class="box box-default">
-    <div class="row">
-        <div class="col-md-6">
-            <div class="box box-primary">
-            <div class="box-header with-border">
-              <h3 class="box-title">Cadastrar Novo Cliente</h3>
-            </div>
-            <!-- /.box-header -->
-            <!-- form start -->
-            <?=form_open('cliente/salvarNovo','');?>
-              <div class="box-body">
-                <div class="form-group">
-                <?php
-                    echo form_label("Nome", "nome");
-                    echo form_input($nome);
-                    echo form_error("nome");
-                ?>
-                </div>
-                <div class="form-group">
-                <?php
-                    echo form_label("Telefone", "telefone");
-                    echo form_input($telefone);
-                    echo form_error("telefone");
-                ?>
-                </div>
-                <div class="form-group">
-                <?php
-                    echo form_label("Endereço", "endereco");
-                    echo form_input($endereco);
-                    echo form_error("endereco");
-                ?>
-                </div>
-                <div class="form-group">
-                <?php
-                    echo form_label("Data de Nascimento", "data_nascimento");
-                    echo form_input($data_nascimento);
-                    echo form_error("data_nascimento");
-                ?>
-                </div>
-                <div class="form-group">
-                <?php
-                    echo form_label("Observações", "observacao");
-                    echo form_input($obs);
-                    echo form_error("observacao");
-                ?>
-                </div>
 
-              </div>
-              <!-- /.box-body -->
-
-              <div class="box-footer">
-                <?=form_button($submitBtn);?>
-              </div>
-            </form>
-          </div>
+<div class="row">
+    <div class="col-md-6" style="margin-left:25%">
+        <div class="box box-primary">
+        <div class="box-header with-border">
+          <h3 class="box-title">Cadastrar Novo Cliente</h3>
         </div>
+        <!-- /.box-header -->
+        <!-- form start -->
+        <?=form_open('cliente/salvarNovo','');?>
+          <div class="box-body">
+            <div class="form-group">
+            <?php
+                echo form_label("Nome", "nome");
+                echo form_input($nome);
+                echo form_error("nome");
+            ?>
+            </div>
+            <div class="form-group">
+            <?php
+                echo form_label("Telefone", "telefone");
+                echo form_input($telefone);
+                echo form_error("telefone");
+            ?>
+            </div>
+            <div class="form-group">
+            <?php
+                echo form_label("Endereço", "endereco");
+                echo form_input($endereco);
+                echo form_error("endereco");
+            ?>
+            </div>
+            <div class="form-group">
+            <?php
+                echo form_label("Data de Nascimento", "data_nascimento");
+                echo form_input($data_nascimento);
+                echo form_error("data_nascimento");
+            ?>
+            </div>
+            <div class="form-group">
+            <?php
+                echo form_label("Observações", "observacao");
+                echo form_input($obs);
+                echo form_error("observacao");
+            ?>
+            </div>
+
+          </div>
+          <!-- /.box-body -->
+
+          <div class="box-footer">
+            <?=form_button($submitBtn);?>
+          </div>
+        </form>
+      </div>
     </div>
 </div>
