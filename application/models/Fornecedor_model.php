24e9baf1e11a795c3c357be553c93b9d5382691a
Macário Soares <macario.junior@gmail.com>
2016-06-27 15:28:41 -0300

    Iniciando fluxo de fornecedor (Cadastro)

---
+++ application/models/Fornecedor_model.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+Class Fornecedor_model extends CI_Model{
+
+    public function salvarNovo($data){
+        $salvo = $this->db->insert('fornecedores', $data);
+        return $salvo;
+    }
+    public function getFornecedores(){
+        $clientes = $this->db->get('fornecedores');
+        return $clientes->result_array();
+    }
+    // public class deleteFornecedor($id_fornecedor){
+    //     $this->db->where('id', $id_fornecedor)
+    //     $deletado = $this->db->delete('fornecedores');
+    //     retunr $deletado;
+    // }
+}
