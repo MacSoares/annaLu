b6a9b391a4a048726f02d8b436357b24d708e952
Anna Larissa <annalarissa.martins@hotmail.com>
2016-06-27 19:02:13 -0300

    Finalizando CRUD de fornecedores
    Signed-off-by: Macário Soares <macario.junior@gmail.com>

--- application/models/Fornecedor_model.php
+++ application/models/Fornecedor_model.php
@@ -5,13 +5,27 @@ Class Fornecedor_model extends CI_Model{
         $salvo = $this->db->insert('fornecedores', $data);
         return $salvo;
     }
+
     public function getFornecedores(){
         $clientes = $this->db->get('fornecedores');
         return $clientes->result_array();
     }
+
     public function delete($id_fornecedor){
         $this->db->where('id_fornecedor', $id_fornecedor);
         $deletado = $this->db->delete('fornecedores');
         return $deletado;
+    }
+
+    public function getFornecedorById($id){
+        $where = array('id_fornecedor' => $id);
+        $fornecedor = $this->db->get_where('fornecedores', $where);
+        return $fornecedor->row_array();
+    }
+
+    public function updateFornecedor($data, $id){
+        $this->db->where('id_fornecedor', $id);
+        $alterado = $this->db->update('fornecedores', $data);
+        return $alterado;
     }
 }
