ee0d8768c240f396996510ec8f7ab452729cadf2
Anna Larissa <annalarissa.martins@hotmail.com>
2016-06-29 10:55:42 -0300

    Cabeçalho de Aniversariantes do dia

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -13,3 +13,14 @@
       </ol>
     </section>
 
+    <div class="row">
+      <div class="col-md-6" style="margin-left:25%">
+        <div class="box box-primary">
+          <div class="box-header with-border">
+            <h3 class="box-title">Aniversariantes do Dia</h3>
+          </div>
+        <!-- /.box-header -->
+        <!-- form start -->
+        </div>
+      </div>
+    </div>
